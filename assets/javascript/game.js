8f52b88831e0301d441692c6319a5d36fc9b4e4a
lsweeney15 <37424956+lsweeney15@users.noreply.github.com>
2018-05-29 21:53:21 -0400

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,98 +1,76 @@
-// Computer chooses a random letter
-
-// Userpicks a letter using document.onkeyup
-
-// All variables //
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var userGuess = [];
 var computerChoice;
 
-//to initiate the game
-theGame();
 
-function theGame() {
-    var alphabet = "abcdefghijklmnopqrstuvwxyz";
-    var randomLetter = alphabet[Math.floor(Math.random() * 26)];
-    var computerChoice = randomLetter;
+Play();
 
-    // If correct guess:
+function Play() {
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	var randomLetter = alphabet[Math.floor(Math.random() * 26)];
+	var computerChoice = randomLetter;
 
-    // If userGuess equals computerChoice the win counter goes up +1
+	checkIfCorrect();
 
-    // If userGuess equals computerChoice guessesLeft resets to 9
+	function checkIfCorrect() {
 
-    // If userGuess equals computerChoice yourGuesses resets
+		document.onkeyup = function (event) {
 
-    checkIfCorrect();
+			var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
 
-    function checkIfCorrect() {
+			if (event.keyCode < 65 || event.keyCode > 90) {
 
-        document.onkeyup = function (event) {
-
-            //makes user's guess an alphanumeric key and makes guess lowercase
-            var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+				// stops user from guessing a letter twice and stops the guesses counter from being deducted for a previous guess
+			} else if (userGuess.indexOf(userChoice) >= 0) {
 
 
-            //if statement to prevent user from choosing a key that's not part of
-            //the alphabet. a = unicode 65, z = unicode 90.
-            if (event.keyCode < 65 || event.keyCode > 90) {
-                alert("Invalid Entry");
+				// if correct
+			} else if (userChoice === computerChoice) {
 
-                //if the user guesses correctly
-            } else if (userGuess === computerChoice) {
-                console.log("You win."); //test
-                wins = wins + 1;
+				wins = wins + 1;
+				document.getElementById("your-wins").innerHTML = wins;
+
+				resetGame();
 
 
-                resetGame();
+				// if incorrect
+			} else {
+				guessesLeft = guessesLeft - 1;
 
+				document.getElementById("guesses-left").innerHTML = guessesLeft;
+				userGuess.push(userChoice);
 
-                // If incorrect guess:
+				document.getElementById("your-guesses").innerHTML = userGuess;
 
-                // If userGuess does not equal computerChoice guessesLeft goes down -1
+				noGuessesLeft();
+			}
+		}
+	}
 
-                // If userGuess does not equal computerChoice guessesSoFar shows the letter guessed
+	function resetGame() {
+		guessesLeft = 9;
+		userGuess = [];
+		document.getElementById("guesses-left").innerHTML = guessesLeft;
+		document.getElementById("your-guesses").innerHTML = userGuess;
+		theGame();
 
-            } else {
-                guessesLeft = guessesLeft - 1;
+	}
 
-                document.getElementById("guesses-left").innerHTML = guessesLeft;
-                yourGuess.push(userGuess); //append user's choice to array yourGuess
+	function noGuessesLeft() {
+		if (guessesLeft === 0) {
+			alert("You lose! Play Again?");
+			losses = losses + 1
+			document.getElementById("your-losses").innerHTML = losses;
 
+			resetGame();
 
-                document.getElementById("your-guesses").innerHTML = userGuess;
+		} else {
+			checkIfCorrect();
+		}
 
-
-                noGuessesLeft();
-            }
-        }
-    }
-
-    function resetGame() {
-        guessesLeft = 9;
-        userGuess = [];
-        document.getElementById("guesses-left").innerHTML = guessesLeft;
-        document.getElementById("your-guesses").innerHTML = yourGuess;
-        theGame(); //restart the game with new computerChoice.
-
-    }
-
-    function noGuessesLeft() {
-        if (guessesLeft === 0) {
-            losses = losses + 1
-            document.getElementById("your-losses").innerHTML = losses;
-
-            resetGame();
-
-        } else {
-
-            checkIfCorrect();
-        }
-
-    }
+	}
 
 }
-
 
